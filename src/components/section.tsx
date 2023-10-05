5f34f346fb693c896d8f179dcee10ef1f42b8286
Samuelplim <samuel12nerd@gmail.com>
2023-10-05 19:00:01 -0400

    feat propried hidden

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -3,14 +3,17 @@ import React, { ReactNode } from "react";
 interface SectionProps {
   title?: string;
   children: ReactNode;
+  hidden?: boolean;
 }
 
-export const Section: React.FC<SectionProps> = ({ title, children }) => {
+export const Section: React.FC<SectionProps> = ({
+  title,
+  children,
+  hidden,
+}) => {
   return (
-    <section className="bg-dark-400 px-4 py-2">
-      <p className="hover:text-shadow hover:text-light-100 text-light-300 text-2xl">
-        {title}
-      </p>
+    <section className="bg-dark-400 px-4 py-2" hidden={hidden}>
+      <p className=" text-light-300 lg:text-2xl text-xl mb-4">{title}</p>
       {children}
     </section>
   );
