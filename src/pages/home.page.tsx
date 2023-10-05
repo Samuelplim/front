b15065632e5847370455bdf00295919c9a16144e
Samuelplim <samuel12nerd@gmail.com>
2023-10-05 19:00:39 -0400

    updated

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -12,6 +12,11 @@ export const HomePage = () => {
       title: "Abaixo-assinado VAT",
       link: "https://peticaopublica.com.br/pview.aspx?pi=BR135067",
     },
+  ];
+  const redesSociais: LinkProps[] = [
+    { title: "Instagram", link: "https://instagram.com/movimento_vat" },
+    { title: "TikTok", link: "https://www.tiktok.com/@movimento_vat" },
+    { title: "Twitter", link: "https://www.reddit.com/r/movimento_vat/" },
     {
       title: "WhatsApp",
       link: "https://chat.whatsapp.com/Klb3kit9NViKH3oFhdEPc8",
@@ -20,15 +25,6 @@ export const HomePage = () => {
       title: "Telegram",
       link: "https://t.me/+-6FKOpL3K4diMjEx",
     },
-    {
-      title: "Enquete - Onde você mora?",
-      link: "https://forms.gle/VBWbr2DFmpZvro4r5",
-    },
-  ];
-  const redesSociais: LinkProps[] = [
-    { title: "Instagram", link: "https://instagram.com/movimento_vat" },
-    { title: "TikTok", link: "https://www.tiktok.com/@movimento_vat" },
-    { title: "Twitter", link: "https://www.reddit.com/r/movimento_vat/" },
   ];
   const gruposWpp: LinkProps[] = [
     { title: "SP", link: "https://chat.whatsapp.com/FrCpQSiWaJlHyw2ERjEiO5" },
@@ -61,65 +57,96 @@ export const HomePage = () => {
   ];
   const tiktoksApoiadores = [
     "https://www.tiktok.com/@andressahcatty/video/7284070820505324805",
+    "https://www.tiktok.com/@juru.ba/video/7286099868383317254",
+    "https://www.tiktok.com/@fhelipe..medeiros/video/7285745376026529029",
+    "https://www.tiktok.com/@francisquinhom/video/7285801308282621189",
+    "https://www.tiktok.com/@samuelbscar/video/7285768666333891846",
+    "https://www.tiktok.com/@mariicvidigal/video/7286438361747754246",
   ];
-  return (
-    <>
-      <main className="bg-dark-400 px-10 h-screen justify-center items-center">
-        <nav className="flex gap-6">
-          <ButtonNav title="sobre" />
-          <ButtonNav title="redes sociais" />
-          <ButtonNav title="grupos regionais" />
-        </nav>
-        <section>
-          <p className="text-xl"> Somo a VAT - Vida Além do trabalho</p>
-          <p>
-            A (VAT) é mais do que apenas um movimento. Somos uma voz unida que
-            ecoa em prol da dignidade no trabaalho e na justiça salarial.
-          </p>
-          <p>
-            Nascemos da necessidade de lutar contra a opressão, a escala 6x1 e
-            as jornadas de trabalho abusivas que tantos enfrentam diariamente.
-          </p>
-          <p>
-            Nossa missão é clara: defender todos os trabalhadores, não importa
-            onde estejam, quem são ou o que fazem. Acreditamos que o trabalho
-            deve enriquecer vidas, não esgotá-las.
-          </p>
-          <p>
-            Somos a revolução que desafia as normas injustas e seremos sempre a
-            resistência contra a exploração
-          </p>
-        </section>
-        <section>
-          <ul>
-            <p>Lutamos pelo fim da escala 6x1</p>
-            <p>Pelo fim da jornada de trbalho abusiva</p>
-            <p>Pela liberdade do trabalhador</p>
-          </ul>
-        </section>
 
-        <section hidden>
-          {links.map((item) => {
-            return <p>{item.title}</p>;
-          })}
-          {redesSociais.map((item) => {
-            return <p>{item.title}</p>;
-          })}
-          <div className="flex flex-col gap-6">
-            <p>Entre no grupo do WhatsApp do seu estado</p>
-            <div className="flex gap-4">
-              {gruposWpp.map((item) => {
-                return <p>{item.title}</p>;
-              })}
-            </div>
+  const _renderRedesSociais = (item: LinkProps) => {
+    return (
+      <a
+        href={item.link}
+        target="_blank"
+        className="bg-dark-1000 px-2 rounded-sm"
+      >
+        <p className="text-light-300 text-sm">{item.title}</p>
+      </a>
+    );
+  };
+
+  return (
+    <main className="bg-dark-400 justify-center items-center">
+      <nav className="flex gap-6">
+        <ButtonNav title="sobre" />
+        <ButtonNav title="grupos regionais" />
+      </nav>
+      <div className="px-4 my-4 bg-dark-100">
+        <p>CONHEÇA NOSSAS REDES SOCIAIS</p>
+        <div className="flex gap-2 mt-2">
+          {redesSociais.map(_renderRedesSociais)}
+        </div>
+      </div>
+      <Section title="Somo a VAT - Vida Além do trabalho">
+        <p>
+          A (VAT) é mais do que apenas um movimento. Somos uma voz unida que
+          ecoa em prol da dignidade no trabalho e na justiça salarial. Nascemos
+          da necessidade de lutar contra a opressão, a escala 6x1 e as jornadas
+          de trabalho abusivas que tantos enfrentam diariamente.
+        </p>
+        <div className="mt-4">
+          <p className="text-lg text-light-100">Missão</p>
+          <p>
+            defender todos os trabalhadores, não importa onde estejam, quem são
+            ou o que fazem. Acreditamos que o trabalho deve enriquecer vidas,
+            não esgotá-las.
+          </p>
+        </div>
+
+        <div className="py-2">
+          <p className="text-sm py-2">
+            Somos a revolução que desafia as normas injustas e seremos sempre a
+            resistência contra a exploração.
+          </p>
+          <ul>
+            <li>- Lutamos pelo fim da escala 6x1</li>
+            <li>- Pelo fim da jornada de trabalho abusiva</li>
+            <li>- Pela liberdade do trabalhador</li>
+          </ul>
+        </div>
+      </Section>
+
+      <Section title="Links">
+        {links.map((item) => {
+          return (
+            <a href={item.link}>
+              <p>{item.title}</p>
+            </a>
+          );
+        })}
+
+        <div className="flex flex-col gap-6">
+          <p>Entre no grupo do WhatsApp do seu estado</p>
+          <div className="flex gap-4">
+            {gruposWpp.map((item) => {
+              return (
+                <a href={item.link}>
+                  <p>{item.title}</p>
+                </a>
+              );
+            })}
           </div>
-        </section>
-        <Section title="Apoiadores da causa">
+        </div>
+      </Section>
+      <Section title="Apoiadores da causa" hidden>
+        <p>Conheça alguns apoiadores do projeto VAT no TikTok</p>
+        <div className="flex">
           {tiktoksApoiadores.map((item, index) => (
             <TikTokVideo key={index} url={item} />
           ))}
-        </Section>
-      </main>
-    </>
+        </div>
+      </Section>
+    </main>
   );
 };
